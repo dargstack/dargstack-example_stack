dbcddf4c6d139c04c7b204742cbb02f3f3da1046
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-03-27 09:34:26 +0200

    feat!: rework for nuxt and edge deployment

--- src/development/certificates/mkcert.sh
+++ src/development/certificates/mkcert.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
-THIS=$(dirname $(readlink -f "$0"))
-NAME=dargstack-example
+THIS=$(dirname "$(readlink -f "$0")")
 
-mkcert \
--cert-file "$THIS/$NAME.crt" \
--ecdsa \
--key-file "$THIS/$NAME.key" \
-    $NAME.test \
-    "*.$NAME.test"
+create() {
+    NAME="$1"
+    shift
+    CONTENT=$*
+
+    path="$THIS/$NAME"
+    certfile="$path.crt"
+    keyfile="$path.key"
+
+    if [ "$CONTENT" != "" ]; then
+        # shellcheck disable=SC2086
+        mkcert \
+            -cert-file "$certfile" \
+            -ecdsa \
+            -key-file "$keyfile" $CONTENT
+    fi
+
+    cat "$(mkcert -CAROOT)/rootCA.pem" >> "$certfile"
+}
+
+echo "key crt" | tr ' ' '\n' | while read -r glob; do
+    if test -n "$(find "$THIS" -maxdepth 1 -name "*.$glob" -print -quit)"; then
+        rm "$THIS"/*."$glob"
+    fi
+done
+
+create "root"
+create "traefik" \
+    `# dargstack-example` "localhost" "www.localhost" "127.0.0.1" "0.0.0.0"
